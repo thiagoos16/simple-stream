2c43b422891472aa6f84f2a72e3abd185a700890
Thiago Oliveira <Thiago.Oliveira2@fit-tecnologia.org.br>
2023-06-07 19:25:27 -0400

    add adjusts to consume full stream

--- streams/stream-http-server.js
+++ streams/stream-http-server.js
@@ -15,10 +15,21 @@ class UnModuleNumberStream extends Transform {
 // req ==> ReadableStream
 // res ==> WritableStream
 
-const server = http.createServer((req, res) => {
-    return req
-        .pipe(new UnModuleNumberStream())
-        .pipe(res)
+const server = http.createServer(async (req, res) => {
+    const buffers = []
+
+    for await (const chunk of req) {
+        buffers.push(chunk)
+    }
+    
+    const fullStreamContent = Buffer.concat(buffers).toString()
+
+    console.log(fullStreamContent)
+
+    return res.end(fullStreamContent)
+    // return req
+    //     .pipe(new UnModuleNumberStream())
+    //     .pipe(res)
 })
 
 server.listen(3334)
