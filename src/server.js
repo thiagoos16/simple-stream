9af36f39a61c473a1ba73e3116e4b3386be9a179
Thiago Oliveira <Thiago.Oliveira2@fit-tecnologia.org.br>
2023-06-09 16:51:25 -0400

    add json middleware

--- src/server.js
+++ src/server.js
@@ -1,7 +1,31 @@
 import http from 'node:http'
+import { json } from '../src/middlewares/json.js'
 
-const server = http.createServer((req, res) => {
-    return res.end("Hello World");
+const users = []
+
+const server = http.createServer(async (req, res) => {
+    const { method, url } = req
+
+    await json(req, res)
+
+    if (method === 'GET' && url === '/users') {
+        return res
+            .end(JSON.stringify(users))
+    }
+
+    if (method === 'POST' && url === '/users') {
+        const { name, email } = req.body
+
+        users.push({
+            id: 1,
+            name,
+            email,
+        })
+
+        return res.writeHead(201).end()
+    }
+
+    return res.writeHead(404).end()
 })
 
 server.listen(3333)
